621da0ad53e9db0959427e6a3ad859f26a99a6ac
YeganaAbd <abd.yegane@gmail.com>
2022-02-09 14:32:42 +0000

    useState added

--- naughts_crosses/src/Components/App/App.js
+++ naughts_crosses/src/Components/App/App.js
@@ -1,11 +1,23 @@
-
-import './App.css';
+import "./App.css";
 import Board from "../Board/index";
+import { useState } from "react";
 
 function App() {
+  let x = "x";
+  let o = "o";
+  const [player, setPlayer] = useState("");
+
+  function handleBoardClick() {
+    if (x) {
+      setPlayer(o);
+      console.log(player);
+    } else if ("" || o) {
+      setPlayer(x);
+    }
+  }
   return (
     <div className="App">
-      <Board />
+      <Board onClick={handleBoardClick} value={player} />
     </div>
   );
 }
--- naughts_crosses/src/Components/Board/index.js
+++ naughts_crosses/src/Components/Board/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./style.css";
 
-export default function Board() {
-      return (
-            <div className="board">
-                  <div className="square"></div>
-                  <div className="square"></div>
-                  <div className="square"></div>
-                  <div className="square"></div>
-                  <div className="square"> </div>
-                  <div className="square"> </div>
-                  <div className="square"> </div>
-                  <div className="square"> </div>
-                  <div className="square"> </div>
-            </div>
-      );
+export default function Board({ onClick, value }) {
+  return (
+    <div onClick={onClick} className="board">
+      <div onClick={onClick} value={value} className="square"></div>
+      <div value={value} className="square"></div>
+      <div value={value} className="square"></div>
+      <div value={value} className="square"></div>
+      <div value={value} className="square"></div>
+      <div value={value} className="square"></div>
+      <div value={value} className="square"></div>
+      <div value={value} className="square"></div>
+      <div value={value} className="square"></div>
+    </div>
+  );
 }
